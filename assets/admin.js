f4ce977c4a7a5fb7df3500e721137a12e30a8211
Josia Scheytt <jscheytt@transline.de>
2020-09-17 17:36:23 +0200

    feat: add click handlers to titles list

--- assets/admin.js
+++ assets/admin.js
@@ -1,32 +1,45 @@
-const dom_audio = document.querySelector('audio');
-// LRC file has the same name as the audio file
-const lyric_url = dom_audio.querySelector('source').src.replace(/\..+?$/,  '.lrc');
+const dom_audio = document.querySelector("audio");
+var lyric;
+
+// Add audio handlers
+dom_audio.onplay = function () {
+  lyric.play(dom_audio.currentTime * 1000);
+};
+dom_audio.onpause = function () {
+  lyric.pause();
+};
+
+// Update audio element file
+const dom_titles = document.querySelectorAll("ul.titles li a");
+dom_titles.forEach(
+  (dom_title) =>
+    (dom_title.onclick = function () {
+      dom_audio.src = `media/${this.text}.mp3`;
+      set_lyric();
+      dom_audio.play();
+    })
+);
 
 // Wait for the websocket handshake to succeed
-ws.onopen = function() {
-  const lyric = new Lyric({
-    onPlay: function(line, text) {
+ws.onopen = function () {
+  lyric = new Lyric({
+    onPlay: function (line, text) {
       ws.send(text);
       console.log(line, text);
     },
-    onSetLyric: function(lines) {
-      // console.log(lines);
-      // ws.send(lines);
-    }
+    onSetLyric: function (lines) {
+      // console.log(lines)
+      // ws.send(lines)
+    },
   });
-
-  fetch(lyric_url)
-    .then((response) => response.text()
-      .then(function(text) {
-        lyric.setLyric(text);
-      }
-    ));
-
-  // Add audio handlers
-  dom_audio.onplay = function() {
-    lyric.play(dom_audio.currentTime * 1000);
-  };
-  dom_audio.onpause = function() {
-    lyric.pause();
-  };
 };
+
+function set_lyric() {
+  // LRC file has the same name as the audio file
+  var lyric_url = dom_audio.src.replace(/\..+?$/, ".lrc");
+  fetch(lyric_url).then((response) =>
+    response.text().then(function (text) {
+      lyric.setLyric(text);
+    })
+  );
+}
